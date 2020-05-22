0f9b005ada4fa952cd484b2f90a931c53a9fa099
michael thomas <michael.thomas2014@gmail.com>
2020-05-22 16:20:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
   def kind 
     if @length_one == @length_two && @length_one == @length_three
       :equilateral
-    elsif 
+    elsif @length_one == @length_two || @length_one == @length_two || @length_one == @length_two
     
     
 end
